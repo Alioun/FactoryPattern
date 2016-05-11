2d612fe5586f0bc13ad7a420ccad1eee21797e50
Alioun Diagne <diagne@hm.edu>
2016-05-11 22:02:51 +0200

    Implemented glorious FakeCounterFactory

--- src/edu/hm/diagne/arch/factory_pattern/FakeCounterFactory.java
+++ src/edu/hm/diagne/arch/factory_pattern/FakeCounterFactory.java
@@ -1,7 +1,18 @@
 package edu.hm.diagne.arch.factory_pattern;
 
+import edu.hm.cs.rs.arch.a03_decorator.Counter;
+import edu.hm.ffrank.arch.decorator_pattern.LoopCounter;
+
 /**
  * Created by diagne on 11/05/16.
  */
 public class FakeCounterFactory extends CounterFactory {
+
+    public Counter make(String typename, int... args){
+        return new LoopCounter(0);
+    }
+
+    public Counter make(Counter other, String typename, int arg) {
+        return new LoopCounter(0);
+    }
 }
